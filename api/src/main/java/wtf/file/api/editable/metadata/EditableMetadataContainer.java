f2e013a2b91a12f676042ed8544756ce335aaac4
lian-hsc <lian.hoerschlaeger@gmail.com>
2025-05-05 18:17:48 +0200

    MetadataContainer and EditableMetadataContainer javadoc

--- api/src/main/java/wtf/file/api/editable/metadata/EditableMetadataContainer.java
+++ api/src/main/java/wtf/file/api/editable/metadata/EditableMetadataContainer.java
@@ -3,20 +3,32 @@ package wtf.file.api.editable.metadata;
 import wtf.file.api.exception.IllegalCharacterException;
 import wtf.file.api.metadata.MetadataContainer;
 
+/**
+ * Represents a container for editable metadata.
+ * <p>
+ * This interface extends the {@link MetadataContainer} and provides additional methods for modifying the metadata,
+ * including setting and removing metadata entries.
+ */
 public interface EditableMetadataContainer extends MetadataContainer {
 
     /**
+     * Removes the metadata entry associated with the specified key from the container.
+     * If the {@code key} is not in the container, this method does nothing.
      *
-     * @param key
-     * @throws IllegalCharacterException
+     * @param key the key of the metadata entry to be removed
      */
-    void remove(String key) throws IllegalCharacterException;
+    void remove(String key);
 
     /**
+     * Sets the value of a metadata entry associated with the specified key.
+     * If a metadata entry with the specified key already exists, its value is updated.
+     * If the specified key or value contains illegal characters, an exception is thrown.
      *
-     * @param key
-     * @param value
-     * @throws IllegalCharacterException
+     * @param key the key of the metadata entry to be set, only ASCII characters,
+     *            excluding the NUL characters, are allowed.
+     * @param value the value to be associated with the specified key, any UTF-8 encodable characters,
+     *              excluding the NUL characters, are allowed.
+     * @throws IllegalCharacterException if the key or value contains one or more illegal characters
      */
     void set(String key, String value) throws IllegalCharacterException;
 
--- api/src/main/java/wtf/file/api/metadata/MetadataContainer.java
+++ api/src/main/java/wtf/file/api/metadata/MetadataContainer.java
@@ -1,16 +1,47 @@
 package wtf.file.api.metadata;
 
+import wtf.file.api.editable.metadata.EditableMetadataContainer;
+
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a container for metadata.
+ * This container allows for the retrieval and inspection of metadata elements identified by keys.
+ *
+ * @see EditableMetadataContainer
+ */
 public interface MetadataContainer {
 
+    /**
+     * Checks if the metadata container contains a value associated with the specified key.
+     *
+     * @param key the key to check for existence in the metadata container
+     * @return true if a value associated with the specified key exists, false otherwise
+     */
     boolean has(String key);
 
+    /**
+     * Retrieves a list of all keys present in the metadata container.
+     *
+     * @return a list of strings representing all keys in the metadata container
+     */
     List<String> keys();
 
+    /**
+     * Retrieves the value associated with the specified key in the metadata container.
+     *
+     * @param key the key for which the associated value is to be retrieved
+     * @return the value associated with the specified key, or null if no value is present
+     */
     String get(String key);
 
+    /**
+     * Retrieves all metadata elements as a map where the keys represent metadata keys
+     * and the values represent their corresponding metadata values.
+     *
+     * @return a map containing all metadata elements, with keys as metadata keys and values as metadata values
+     */
     Map<String, String> asMap();
 
 }
